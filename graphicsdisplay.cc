9622c3d91adba1d0bbb7d80b81eaab3f4def2899
ryanharrs <ryanharrs@gmail.com>
2017-12-04 03:50:57 -0500

    same

--- graphicsdisplay.cc
+++ graphicsdisplay.cc
@@ -17,26 +17,85 @@ GraphicsDisplay::GraphicsDisplay():
 }
 
 void GraphicsDisplay::updateScore(int newScore){
-	string score = to_string(newScore);
-	xw.fillRectangle(190, 24, 50, 20, Xwindow::White);
-	xw.drawString(190, 24, score);
+	score = to_string(newScore);
+	xw.clearArea(0, 0, 0, 30);
+ 	xw.drawString(10,24, "Level:");
+	xw.drawString(80,24, level);
+	xw.drawString(130, 24, "Score:");
+	xw.drawString(190,24, score);
+	xw.drawString(260,24, "Hi Score:");
+	xw.drawString(340,24, highScore);
 
 }
 
 void GraphicsDisplay::updateHighScore(int newHighScore){
-	string highScore = to_string(newHighScore);
-	xw.fillRectangle(340, 24, 50, 20, Xwindow::White);
-	xw.drawString(190, 24, highScore);
+	highScore = to_string(newHighScore);
+	xw.clearArea(0, 0, 0, 30);
+ 	xw.drawString(10,24, "Level:");
+	xw.drawString(80,24, level);
+	xw.drawString(130, 24, "Score:");
+	xw.drawString(190,24, score);
+	xw.drawString(260,24, "Hi Score:");
+	xw.drawString(340,24, highScore);
 
 }
 
 void GraphicsDisplay::updateLevel(int newLevel){
-	string level = to_string(newLevel);
-	xw.fillRectangle(190, 24, 50, 20, Xwindow::White);
-	xw.drawString(10, 24, level);
+	level = to_string(newLevel);
+	xw.clearArea(0, 0, 0, 30);
+ 	xw.drawString(10,24, "Level:");
+	xw.drawString(80,24, level);
+	xw.drawString(130, 24, "Score:");
+	xw.drawString(190,24, score);
+	xw.drawString(260,24, "Hi Score:");
+	xw.drawString(340,24, highScore);
 
 }
 
+void GraphicsDisplay::updateNextBlock(Block &b){
+	
+	int cellWidth = 30;
+	int cellHeight = 30;
+	int a = widthSize - sidePanelWidth + 20;
+	int r = 15;
+	xw.clearArea(a - 20, cellHeight * 3, 100,100);
+	for(int idx = 0; idx < 4; idx++){
+		auto info = b.getCell(idx);
+		string type = to_string(b.getType());
+	 	switch(b.getType()) {
+  			  case '?':
+			    xw.fillRectangle(info.col * cellWidth + a,(info.row + 1) * cellHeight - r, cellWidth, cellHeight, Xwindow::Black);
+			    break;
+			  case '*':
+			    xw.fillRectangle(info.col * cellWidth + a,(info.row + 1) * cellHeight - r, cellWidth, cellHeight, Xwindow::Brown);
+			    break;
+			  case 'S':
+			    xw.fillRectangle(info.col * cellWidth+ a,(info.row + 1) * cellHeight- r, cellWidth, cellHeight, Xwindow::Green);
+			    break;
+			  case 'I':
+			    xw.fillRectangle(info.col * cellWidth + a, (info.row + 1) * cellHeight - r, cellWidth, cellHeight, Xwindow::Cyan);
+			    break;
+			  case 'J':
+			    xw.fillRectangle(info.col * cellWidth + a, (info.row + 1)* cellHeight- r, cellWidth, cellHeight, Xwindow::Orange);
+			    break;
+		  	  case 'L':
+			    xw.fillRectangle(info.col * cellWidth + a, (info.row + 1) * cellHeight - r, cellWidth, cellHeight, Xwindow::Blue);
+			    break;
+			  case 'O':
+			    xw.fillRectangle(info.col * cellWidth + a, (info.row + 1) * cellHeight - r, cellWidth, cellHeight, Xwindow::Yellow);
+			    break;
+			  case 'Z':
+			    xw.fillRectangle(info.col * cellWidth + a, (info.row + 1) * cellHeight - r, cellWidth, cellHeight,Xwindow::Red);
+			    break;
+			  case 'T':
+			    xw.fillRectangle(info.col * cellWidth + a, (info.row + 1)* cellHeight - r, cellWidth, cellHeight, Xwindow::Purple);
+			    break;
+			  default:
+			    xw.fillRectangle(info.col * cellWidth + a, (info.row + 1) * cellHeight - r, cellWidth, cellHeight, Xwindow::Gray);
+  }
+	
+	}
+}
 
 void GraphicsDisplay::notify(Subject<Info> &whoNotified) {
   auto info = whoNotified.getInfo();
